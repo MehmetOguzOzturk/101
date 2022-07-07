3cf42bd2e1326b204665099a04ac8b9969f35fef
oguzoztruk <mhmtozt22@gmail.com>
2022-07-07 18:13:49 +0300

    2 Taş ve ıstakayı yaptım

---
+++ 101 Okey/Assets/Code/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<HoldAndDrag> dragAbleObjects;
+    public List<Transform> snapPoints;
+    public float snapRange = 0.5f;
+    void Start()
+    {
+        foreach (HoldAndDrag dragable in dragAbleObjects)
+        {
+            dragable.dragEndenCallBack = OnDragEnded;
+        }
+    }
+
+   void OnDragEnded(HoldAndDrag dragable)
+    {
+        float closestDistance = -1;
+        Transform closestSnapPoint = null;
+
+        foreach (Transform snapPos in snapPoints)
+        {
+            float currenDistance = Vector2.Distance(dragable.transform.localPosition, snapPos.transform.localPosition);
+
+            if (closestSnapPoint==null || currenDistance<closestDistance)
+            {
+                closestSnapPoint = snapPos;
+                closestDistance = currenDistance;
+                
+            }
+        }
+
+        if (closestSnapPoint!=null && closestDistance<=snapRange)
+        {
+            dragable.transform.localPosition = closestSnapPoint.localPosition;
+        }
+    }
+}
---
+++ 101 Okey/Assets/Code/HoldAndDrag.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldAndDrag : MonoBehaviour
+{
+    float startPosX;
+    float startPosY;
+    float startPosZ;
+    bool isHold;
+
+    public delegate void DragEndenDelegete(HoldAndDrag dragableObject);
+
+    public DragEndenDelegete dragEndenCallBack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHold )
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, transform.position.z);
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - gameObject.transform.position.x;
+            startPosY = mousePos.y - gameObject.transform.position.y;
+            
+
+            isHold = true;
+
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isHold = false;
+        dragEndenCallBack(this);
+    }
+}
